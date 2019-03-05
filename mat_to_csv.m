80e99e1fccca1b2a4ce490cee9bfae90160353a5
luisacfl <luisa.fl.97@gmail.com>
2019-03-04 22:06:22 -0600

    Limpieza de datos y primer análisis.

---
+++ mat_to_csv.m
@@ -0,0 +1,18 @@
+FileData = load('datafinal_2012.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
+FileData = load('datafinal_2013.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
+FileData = load('datafinal_2014.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
+FileData = load('datafinal_2015.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
+FileData = load('datafinal_2016.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
+FileData = load('datafinal_2017.mat');
+csvwrite('datafinal_2013.csv', FileData.M);
+
