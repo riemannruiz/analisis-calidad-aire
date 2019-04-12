b7f6ea3fafb0b3343a11273879e5e8877a477139
riemannruiz <riemannruiz@iteso.mx>
2019-04-12 13:04:27 -0500

    Se agregó el calculo del RMSE para todas las estaciones para O3 y PM10

--- Code_PTI/main4_v2_PTI.m
+++ Code_PTI/main4_v2_PTI.m
@@ -24,7 +24,7 @@ figure(1);
 plot(T,O3_all{idx,2+n},'b-',T,O3_predict_v1(:,n),'r-',T,O3_predict_v2(:,n),'g-'),grid;
 
 figure(2);
-plot(T,PM10_all{idx,3},'b-',T,PM10_predict_v1(:,n),'r-',T,PM10_predict_v2(:,n),'g-'),grid;
+plot(T,PM10_all{idx,2+n},'b-',T,PM10_predict_v1(:,n),'r-',T,PM10_predict_v2(:,n),'g-'),grid;
 
 
 %% simulacion del modelo lineal retroalimentado con los datos de anio seleccionado
@@ -42,4 +42,17 @@ figure(3);
 plot(T,O3_all{idx,2+n},'b-',T,O3_predict_v1(:,n),'r-',T,O3_predict_retro_v1(:,n),'g-'),grid;
 
 figure(4);
-plot(T,PM10_all{idx,3},'b-',T,PM10_predict_v1(:,n),'r-',T,PM10_predict_retro_v1(:,n),'g-'),grid;
+plot(T,PM10_all{idx,2+n},'b-',T,PM10_predict_v1(:,n),'r-',T,PM10_predict_retro_v1(:,n),'g-'),grid;
+
+
+%% Calculo de los errores para evaluar los desempenos del predictor de O3
+O3_RMSE_p_v1 = nanmean((O3_all{idx,3:12}-O3_predict_v1).^2);
+O3_RMSE_p_v2 = nanmean((O3_all{idx,3:12}-O3_predict_v2).^2);
+O3_RMSE_pr_v1 = nanmean((O3_all{idx,3:12}-O3_predict_retro_v1).^2);
+O3_RMSE_pr_v2 = nanmean((O3_all{idx,3:12}-O3_predict_retro_v2).^2);
+
+%% Calculo de los errores para evaluar los desempenos del predictor de PM10
+PM10_RMSE_p_v1 = nanmean((PM10_all{idx,3:12}-PM10_predict_v1).^2);
+PM10_RMSE_p_v2 = nanmean((PM10_all{idx,3:12}-PM10_predict_v2).^2);
+PM10_RMSE_pr_v1 = nanmean((PM10_all{idx,3:12}-PM10_predict_retro_v1).^2);
+PM10_RMSE_pr_v2 = nanmean((PM10_all{idx,3:12}-PM10_predict_retro_v2).^2);
